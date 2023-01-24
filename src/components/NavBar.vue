a6d3bc6e6e28291ff8b0d04fe4154af69a2b5270
nckdgns3167 <nckdgns3167@naver.com>
2023-01-24 17:17:38 +0900

    NavBar 컴포넌트 추가

---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="nav">
+    <router-link to="/news">News</router-link> | <router-link to="/ask">Ask</router-link> |
+    <router-link to="/jobs">Jobs</router-link>
+  </div>
+</template>
+
+<style scoped>
+.nav {
+  color: white;
+  background-color: #42b883;
+  display: flex;
+  padding: 8px;
+}
+
+.nav .router-link-exact-active {
+  color: #35495e;
+}
+
+.nav a {
+  color: white;
+}
+</style>
